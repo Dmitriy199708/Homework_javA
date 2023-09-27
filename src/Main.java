39b8536962608a48ff1cccbe72e988a7a8847114
Dmitriy199708 <119492971+Dmitriy199708@users.noreply.github.com>
2023-09-27 20:41:10 +0300

    add repository

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int amountSpentOnTicket = 25899;
+        int bonus = 20;
+
+
+        int totalNumberOfBonuses =  amountSpentOnTicket / bonus;
+
+        System.out.println("Итоговое кол-во бонусов " + totalNumberOfBonuses);
+
+    }
+}
